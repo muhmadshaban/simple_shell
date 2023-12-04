919d8ec782b620c8a66a7acbe2cb854060256a14
muhmadshaban <moshaban96.eng@gmail.com>
2023-12-04 23:36:56 +0200

    custom_shell.h

--- custom_shell.h
+++ custom_shell.h
@@ -109,123 +109,100 @@ typedef struct builtin
 	int (*func)(info_t *);
 } builtin_table;
 
-	int check_interactive(info_t *info)
-	int is_separator(char c, char *separator)
-	int is_alpha_char(int c)
-	int str_to_int(char *s)
-	int display_environment(__attribute__((unused)) char **command, __attribute__((unused)) int status)
-	int display_help_info(char **command, __attribute__((unused)) int status)
-	int execute_echo(char **command, int status)
-	int display_history(__attribute__((unused)) char **command, __attribute__((unused)) int status)
-	int execute_normal_echo(char **command)
-	int display_command_history(info_t *info)
-	int clear_alias(info_t *info, char *alias_string)
-	int create_alias(info_t *info, char *alias_string)
-	int print_alias_command(list_t *node)
-	int display_aliases(info_t *info)
-	void exit_shell(char **cmd, char *input, char **argv, int c)
-	int change_directory(char **cmd, __attribute__((unused))int er)
-	int deallocate_pointer(void **ptr)
-	int print_custom_environment(info_t *info)
-	char *get_custom_env_var(info_t *info, const char *name)
-	int set_custom_env_var(info_t *info)
-	int unset_custom_env_var(info_t *info)
-	int populate_custom_env_list(info_t *info)
-	void _custom_eputs(char *str)
-	int _custom_eputchar(char c)
-	int _custom_putfd(char c, int fd)
-	int _custom_putsfd(char *str, int fd)
-	int _custom_erratoi(char *s)
-	void custom_print_error(info_t *info, char *estr)
-	int custom_print_d(int input, int fd)
-	char *custom_convert_number(long int num, int base, int flags)
-	void custom_remove_comments(char *buf)
-	int custom_handle_builtin(char **cmd, int er)
-	int custom_check_cmd(char **cmd, char *input, int c, char **argv)
-	void custom_signal_to_handle(int sig)
-	void custom_signal_handler(int sig)
-	void custom_read_file(char *filename, char **argv)
-	void custom_treat_file(char *line, int counter, FILE *fp, char **argv)
-	void custom_exit_built_in_for_file(char **cmd, char *line, FILE *fd)
-	char *custom_strncpy(char *dest, const char *src, size_t n)
-	char *custom_strncat(char *dest, const char *src, size_t n)
-	char *custom_strchr(const char *s, int c)
-	int custom_path_command(char **cmd)
-	char *custom_build(char *token, char *value)
-	char *custom_getenv(char *name)
-	char **custom_get_environment(info_t *info)
-	int custom_unset_environment(info_t *info, char *env_var)
-	int custom_set_environment_variable(info_t *info, char *variable, char *value)
-	void custom_initialize_info(custom_info_t *info)
-	void custom_set_info(custom_info_t *info, char **av)
-	void custom_free_info(custom_info_t *info, int all)
-	void custom_initialize_info(custom_info_t *info)
-	void custom_set_info(custom_info_t *info, char **av)
-	void custom_free_info(custom_info_t *info, int all)
-	ssize_t custom_input_buffer(custom_info_t *info, char **buffer, size_t *buffer_length)
-	ssize_t custom_get_input(custom_info_t *info)
-	ssize_t custom_read_buffer(custom_info_t *info, char *buffer, size_t *buffer_size)
-	int custom_get_line(custom_info_t *info, char **buffer_ptr, size_t *buffer_length)
-	void custom_sigintHandler(__attribute__((unused))int sig_num)
-	char *custom_get_history_file(custom_info_t *info)
-	int custom_write_history(custom_info_t *info)
-	int custom_read_history(custom_info_t *info)
-	int custom_build_history_list(custom_info_t *info, char *buf, int line_count)
-	int custom_renumber_history(custom_info_t *info)
-	list_t *custom_add_node(list_t **head, const char *str, int num)
-	list_t *custom_add_node_end(list_t **head, const char *str, int num)
-	size_t custom_print_list_str(const list_t *head)
-	int custom_delete_node_at_index(list_t **head, unsigned int index)
-	void custom_free_list(list_t **head_pointer)
-	size_t custom_get_list_length(const list_t *head)
-	char **custom_list_to_strings(list_t *head)
-	size_t custom_print_list(const list_t *head)
-	list_t *custom_node_starts_with(list_t *node, char *prefix, char c)
-	ssize_t custom_get_node_index(list_t *head, list_t *node)
-	void *custom_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
-	void custom_free_all(char **cmd, char *line)
-	char *custom_memcpy(char *dest, char *src, unsigned int n)
-	void *custom_fill_array(void *a, int el, unsigned int len)
-	void *custom_calloc(unsigned int size)
-	int custom_check_builtin(char **cmd)
-	void custom_create_environment(char **envi)
-	int custom_is_executable_command(info_t *info, char *file_path)
-	char *custom_duplicate_chars(char *path_str, int start_index, int stop_index)
-	char *custom_find_path(info_t *info, char *path_str, char *cmd)
-	int custom_putchar(char c)
-	void custom_print_error(char *input, int counter, char **argv)
-	char **custom_parse_cmd(char *input)
-	char *custom_memset_fill(char *dest, char fill_byte, unsigned int num_bytes)
-	void *custom_realloc_mem(void *ptr, unsigned int old_size, unsigned int new_size)
-	int custom_shell(info_t *info, char **av)
-	int custom_find_builtin(info_t *info)
-	void custom_find_cmd(info_t *info)
-	void custom_fork_cmd(info_t *info)
-	char *custom_strcpy(char *dest, char *src)
-	char *custom_strdup(const char *str)
-	void custom_puts(char *str)
-	int custom_putchar(char c)
-	int custom_get_string_length(char *str)
-	int custom_compare_strings(char *str1, char *str2)
-	char *custom_string_starts_with(const char *haystack, const char *needle)
-	char *custom_strcat(char *dest, char *src)
-	char *custom_strcpy(char *dest, char *src)
-	char *custom_strcat(char *dest, char *src)
-	char *custom_strchr(char *s, char c)
-	int custom_strncmp(const char *s1, const char *s2, size_t n)
-	char *custom_strdup(char *str)
-	int custom_isalpha(int c)
-	char *custom_itoa(unsigned int n)
-	void custom_array_rev(char *arr, int len)
-	int custom_intlen(int num)
-	void custom_prerror(char **argv, int c, char **cmd)
-	char **custom_strtow(char *input_str, char *del_s)
-	char **custom_strtow2(char *input_str, char del)
-	int custom_is_chain_del(info_t *info, char *buf, size_t *cur_pos)
-	void custom_chain_status(info_t *info, char *buf, size_t *cur_pos, size_t ist_pos, size_t buf_len)
-	int custom_replace_alias(info_t *info)
-	int custom_replace_vars(info_t *info)
-	int custom_replace_string(char **old, char *new)
+int is_interactive(info_t *info);
+int replace_string(char **, char *);
+int replace_vars(info_t *info);
+int replace_alias(info_t *);
+void check_chain_status(info_t *info, char *buf, size_t *current_position, size_t start_position, size_t buffer_length);
+int is_chain_delimiter(info_t *info, char *buf, size_t *current_position);
+char **strtow(char *, char *);
+char **strtow2(char *, char);
+char *_strcpy(char *dest, char *src);
+int _putchar(char c);
+void print_string(char *str);
+char *concat_strings(char *dest, char *src);
+char *string_starts_with(const char *haystack, const char *needle);
+int compare_strings(char *str1, char *str2);
+int get_string_length(char *str);
+void fork_cmd(info_t *info);
+void find_cmd(info_t *info);
+int find_builtin(info_t *info);
+int hsh(info_t *info, char **av);
+void *realloc_mem(void *ptr, unsigned int old_size, unsigned int new_size);
+void free_str_arr(char **str_arr);
+char *find_path(info_t *info, char *path_str, char *cmd);
+char *duplicate_chars(char *path_str, int start_index, int stop_index);
+int is_executable_command(info_t *info, char *file_path);
+int free_pointer(void **ptr);
+ssize_t get_node_index(list_t *head, list_t *node);
+list_t *node_starts_with(list_t *node, char *prefix, char c);
+size_t print_list(const list_t *head);
+char **list_to_strings(list_t *head);
+void free_list(list_t **head_ptr);
+int delete_node_at_index(list_t **head, unsigned int index);
+size_t print_list_str(const list_t *head);
+list_t *add_node_end(list_t **head, const char *str, int num);
+list_t *add_node(list_t **head, const char *str, int num);
+int renumber_history(info_t *info);
+int build_history_list(info_t *info, char *buf, int line_count);
+int read_history(info_t *info);
+int write_history(info_t *info);
+char *get_history_file(info_t *info);
+void free_info(info_t *info, int all);
+void set_info(info_t *info, char **av);
+void initialize_info(info_t *info);
+int set_environment_variable(info_t *info, char *variable, char *value);
+int unset_env(info_t *info, char *env_var);
+char **get_environment(info_t *info);
+void handle_sigint(int signal_number);
+int get_line(info_t *info, char **buffer_ptr, size_t *buffer_length);
+ssize_t read_buffer(info_t *info, char *buffer, size_t *buffer_size);
+ssize_t get_input(info_t *info);
+ssize_t input_buffer(info_t *info, char **buffer, size_t *buffer_length);
+char *_strncat(char *dest, const char *src, size_t n);
+char *_strncpy(char *dest, const char *src, size_t n);
+void remove_comments(char *buf);
+char *convert_number(long int num, int base, int flags);
+int print_d(int input, int fd);
+void print_error(info_t *info, char *estr);
+int _erratoi(char *s);
+int _putsfd(char *str, int fd);
+int _putfd(char c, int fd);
+int _eputchar(char c);
+void _eputs(char *str);
+int populate_env_list(info_t *info);
+int unset_env_var(info_t *info);
+int set_env_var(info_t *info);
+char *get_env_var(info_t *info, const char *name);
+int print_environment(info_t *info);
+int print_aliases(info_t *info);
+int print_alias(list_t *node);
+int set_alias(info_t *info, char *alias_str);
+int unset_alias(info_t *info, char *alias_str);
+int print_history(info_t *info);
+void chain_stat(info_t *info, char *bug, size_t *cur_pos, size_t ist_pos, size_t buf_len);
+int print_help(info_t *info);
+int change_dir(info_t *info);
+int exit_shell(info_t *info);
+int string_to_int(char *s);
+int is_alphabetic(int c);
+int is_chain_del(info_t *info, char *buf, size_t *cur_pos);
+int is_delimiter(char c, char *delim);
+int is_interactive(info_t *info);
+char **strtow2(char *input_str, char del);
+char **strtow(char *input_str, char *del_s);
+int str_length(const char *string);
+int _strcmp(char *, char *);
+char *starts_with(const char *, const char *);
+char *str_concat(char *dest, char *src);
+char *str_duplicate(const char *string);
+void _puts(char *);
+void _memcpy(void *newptr, const void *ptr, unsigned int size);
+int _putchar(char);
+char *_strchr(char *, char);
+int bfree(void **ptr);
+int free_pointer(void **ptr);
+char _strdup(void **);
 
 
 #endif
