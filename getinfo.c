b1dfbc4979e7777585296283c35d5ecb98036b97
muhmadshaban <moshaban96.eng@gmail.com>
2023-12-11 15:47:18 +0200

    getinfo.c

--- getinfo.c
+++ getinfo.c
@@ -1,4 +1,4 @@
-#include "shell.h"
+#include <shell.h>
 /**
  ** initialize_info - Initializes info
  ** @info: Struct address
@@ -67,7 +67,7 @@ void free_info(info_t *info, int all)
 	_putchar(BUF_FLUSH);
 }
 }
-#include "shell.h"
+#include <shell.h>
 
 /**
  ** clear_info - Initializes info_t struct
